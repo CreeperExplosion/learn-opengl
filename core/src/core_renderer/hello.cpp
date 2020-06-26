97c96d1b3fb7c01ba1f9458b19e60a41614bcb4e
CreeperExplosion <ghibranresearch@gmail.com>
2020-06-26 22:21:42 +0200

    setting up build system

---
+++ core/src/core_renderer/hello.cpp
@@ -0,0 +1,32 @@
+#include "core_renderer/hello.h"
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+void Renderer::SayHello()
+{
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cerr << "GLAD failed" << std::endl;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+}
---
+++ sandbox/src/sandbox/main.cpp
@@ -0,0 +1,8 @@
+
+#include "graphics.h"
+
+int main(int argc, char const *argv[])
+{
+    Renderer::SayHello();
+    return 0;
+}
